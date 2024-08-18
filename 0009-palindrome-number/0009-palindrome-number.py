51f07f95ca71be60fc9febc2c465e26987dd28c2
Tesfamichael Taffere <149233683+Tesfamichael12@users.noreply.github.com>
2024-08-18 22:13:14 +0300

    Time: 48 ms (80.27%), Space: 16.6 MB (38.07%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def isPalindrome(self, x):
+
+        # uisng simple sytax sugar in python
+        x = str(x)
+        return x == x[::-1] #In Python, slicing notation x[start:stop:step] allows us to extract a portion of the sequence x based on the given parameters.
