b49b1170655ced6e292bf4debc6ef4a85fe5fde5
Tesfamichael Taffere <149233683+Tesfamichael12@users.noreply.github.com>
2024-08-19 23:10:52 +0300

    Time: 3 ms (46.85%), Space: 7.8 MB (14.87%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int l = 0, r = 0;
+        while (l < s.length() && r < t.length())
+        {
+            if (s[l] == t[r])
+               l++;
+            r++;
+        }
+
+        return (l == s.length());
+    }
+};
