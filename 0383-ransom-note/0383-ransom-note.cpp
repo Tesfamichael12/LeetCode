e687148a65e71d1636314f6277eb20529c6fca10
Tesfamichael Taffere <149233683+Tesfamichael12@users.noreply.github.com>
2024-08-19 23:10:04 +0300

    Time: 16 ms (57.27%), Space: 11.4 MB (45.76%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char, int> hashmap;
+
+        for (char ch : magazine) {
+            hashmap[ch]++;
+        }
+
+        for (char ch : ransomNote) {
+            if (hashmap[ch] > 0) {
+                hashmap[ch]--;
+            } else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
+
+// Time Complexity: O(m + n)  -> m = length of ransomNote, n = length of magazine
+// Space Complexity: O(n)     -> we're using an unordered_map
