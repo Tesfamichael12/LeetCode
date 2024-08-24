651898c06b0c49c2ac735a39a05507c44b5afaf4
Tesfamichael Taffere <149233683+Tesfamichael12@users.noreply.github.com>
2024-08-24 20:31:09 +0300

    Time: 5598 ms (5.03%), Space: 24.6 MB (83.93%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -5,30 +5,28 @@
 #         self.next = next
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
-        """
-        Do not return anything, modify head in-place instead.
-        """
-        # find the middel of the linked list 
-        slow = fast = head
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
+        if not head:
+            return
+        
+        def recursion(node: Optional[ListNode]) -> Optional[ListNode]:
+            if not node or not node.next:
+                return node
+            
+            lastNode = node
+            prevNode = None
+            
+            while lastNode.next:
+                prevNode = lastNode
+                lastNode = lastNode.next
+            
+            prevNode.next = None
 
-        # reverse the second half part
-        prv = None
-        cur = slow
-        while cur:
-            temp = cur.next
-            cur.next = prv
-            prv = cur
-            cur = temp
+            remainingPart = node.next
+            
+            lastNode.next = recursion(remainingPart)
+            
+            node.next = lastNode
 
-        # Now let's merge the two halfs alternatively
-        first, second = head, prv
-        while second.next:
-            temp1, temp2 = first.next, second.next
+            return node
 
-            first.next = second
-            second.next = temp1
-
-            first, second = temp1, temp2
+        recursion(head)
