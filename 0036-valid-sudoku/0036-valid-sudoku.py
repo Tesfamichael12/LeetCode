6d944269e4cd825e857aaa7d324e0a483944ec29
Tesfamichael Taffere <149233683+Tesfamichael12@users.noreply.github.com>
2024-08-26 01:53:21 +0300

    Time: 92 ms (58.32%), Space: 16.5 MB (84.77%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,34 +1,18 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
+        row = defaultdict(set)
+        col = defaultdict(set)
+        square = defaultdict(set)
+
         for i in range(9):
-            nums_row = set()
-            nums_col = set()
-
             for j in range(9):
-               # check each row
                 num = board[i][j]
-                if num in nums_row:
-                    return False
-                elif num !=".":
-                    nums_row.add(num)
+                s = (i//3, j//3)
 
-               # check each row
-                num = board[j][i]
-                if num in nums_col:
-                    return False
-                elif num != ".":
-                    nums_col.add(num)
-        
-        start = [(0,0), (0,3), (0,6),
-                 (3,0), (3,3), (3,6),
-                 (6,0), (6,3), (6,6)]
-        for i, j in start:
-            nums = set()
-            for row in range(i, i+3):
-                for col in range(j, j+3):
-                    num = board[row][col]
-                    if num in nums:
-                        return False
-                    elif num !=".":
-                        nums.add(num)
+                if board[i][j] == ".": continue
+                if num in row[i] or num in col[j] or num in square[s]: return False
+
+                row[i].add(num)
+                col[j].add(num)
+                square[s].add(num)
         return True
