7619f8285b8789aa48ab007cacf45616ace3ce88
Tesfamichael Taffere <149233683+Tesfamichael12@users.noreply.github.com>
2024-08-26 04:48:26 +0300

    Time: 168 ms (67.57%) | Memory: 18 MB (77.00%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,13 +1,11 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        hashmap = {}
-        for n in nums:
-            hashmap[n] = hashmap[n] + 1 if n in hashmap else 1
-        majority = 1
-        num = nums[0]
-        for n in set(nums):
-            if hashmap[n] > majority:
-                num = n
-                majority = hashmap[n]
-        
-        return num
+        count = Counter(nums)
+        maxval = 1
+        maj = nums[0]
+        for key, val in count.items():
+            if maxval < val:
+                maj = key
+                maxval = val
+
+        return maj
