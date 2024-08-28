194ecb87a9ad18af5336f05e4347e51578835291
Tesfamichael Taffere <149233683+Tesfamichael12@users.noreply.github.com>
2024-08-28 22:49:56 +0300

    Time: 93 ms (52.13%) | Memory: 16.6 MB (13.82%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -2,17 +2,31 @@ class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         row = defaultdict(set)
         col = defaultdict(set)
-        square = defaultdict(set)
+        s = defaultdict(set)
 
         for i in range(9):
             for j in range(9):
-                num = board[i][j]
-                s = (i//3, j//3)
-
                 if board[i][j] == ".": continue
-                if num in row[i] or num in col[j] or num in square[s]: return False
 
-                row[i].add(num)
-                col[j].add(num)
-                square[s].add(num)
+                num = board[i][j]
+                # checking the rows
+                if num in row[i]:
+                    return False
+                else:
+                    row[i].add(num)
+
+                # check the cols
+                if num in col[j]:
+                    return False
+                else:
+                    col[j].add(num)
+
+                # check the squares
+                if num in s[i//3, j//3]:
+                    return False
+                else:
+                    s[i//3, j//3].add(num)
         return True
+
+
+                
