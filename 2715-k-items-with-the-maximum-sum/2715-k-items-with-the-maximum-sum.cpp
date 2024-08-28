857ed998cb737005941abb761aaddb29ef614cb5
Tesfamichael Taffere <149233683+Tesfamichael12@users.noreply.github.com>
2024-08-28 18:30:36 +0300

    Time: 0 ms (100%), Space: 9 MB (64.85%) - LeetHub

---
+++ 2715-k-items-with-the-maximum-sum/2715-k-items-with-the-maximum-sum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
+    if (numOnes >= k) {
+        return k;
+    }
+
+    if (numOnes + numZeros >= k) {
+        return numOnes;
+    }
+
+    return numOnes - (k - (numOnes + numZeros));
+
+    }
+};
