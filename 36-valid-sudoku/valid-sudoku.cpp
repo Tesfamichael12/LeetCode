773e5225e36f713e2f72cc7671fc6cce5d07dd64
Tesfamichael Taffere <149233683+Tesfamichael12@users.noreply.github.com>
2024-08-28 22:59:58 +0300

    Time: 20 ms (39.04%) | Memory: 26.2 MB (18.71%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,48 +1,25 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        // Check rows and columns
+        vector<unordered_set<char>> rows(9), cols(9);
+        vector<unordered_set<char>> subgrids(9);
+
         for (int i = 0; i < 9; ++i) {
-            unordered_set<char> nums_row;
-            unordered_set<char> nums_col;
-
             for (int j = 0; j < 9; ++j) {
-                // Check each row
+                if (board[i][j] == '.') continue; 
+
                 char num = board[i][j];
-                if (nums_row.find(num) != nums_row.end()) {
+                int subgridIndex = (i / 3) * 3 + (j / 3);
+
+                if (rows[i].count(num) || cols[j].count(num) || subgrids[subgridIndex].count(num)) {
                     return false;
-                } else if (num != '.') {
-                    nums_row.insert(num);
                 }
 
-                // Check each column
-                num = board[j][i];
-                if (nums_col.find(num) != nums_col.end()) {
-                    return false;
-                } else if (num != '.') {
-                    nums_col.insert(num);
-                }
+                rows[i].insert(num);
+                cols[j].insert(num);
+                subgrids[subgridIndex].insert(num);
             }
         }
-
-        // Check 3x3 sub-grids
-        vector<pair<int, int>> start = {{0, 0}, {0, 3}, {0, 6}, 
-                                        {3, 0}, {3, 3}, {3, 6}, 
-                                        {6, 0}, {6, 3}, {6, 6}};
-        for (auto [i, j] : start) {
-            unordered_set<char> nums;
-            for (int row = i; row < i + 3; ++row) {
-                for (int col = j; col < j + 3; ++col) {
-                    char num = board[row][col];
-                    if (nums.find(num) != nums.end()) {
-                        return false;
-                    } else if (num != '.') {
-                        nums.insert(num);
-                    }
-                }
-            }
-        }
-
         return true;
     }
 };
