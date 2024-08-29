667b21f2c58be0654160cc1e2ed30a2318d8fd8e
Tesfamichael Taffere <149233683+Tesfamichael12@users.noreply.github.com>
2024-08-29 04:49:59 +0300

    Time: 0 ms (100%), Space: 10.4 MB (43.95%) - LeetHub

---
+++ 1217-relative-sort-array/1217-relative-sort-array.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <iterator>
+
+class Solution {
+public:
+    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        unordered_map<int, int> count;
+        for (int num : arr1) {
+            count[num]++;
+        }
+
+        vector<int> out;
+        for (int num : arr2) {
+            out.insert(out.end(), count[num], num);
+            count.erase(num);
+        }
+
+        vector<pair<int, int>> remaining(count.begin(), count.end());
+        sort(remaining.begin(), remaining.end());
+
+        for (const auto& [num, freq] : remaining) {
+            out.insert(out.end(), freq, num);
+        }
+
+        return out;
+    }
+};
