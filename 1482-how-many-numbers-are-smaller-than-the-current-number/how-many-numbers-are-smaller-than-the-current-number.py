09b84099e5b2f833465145df23fc7dfb7fdba180
Tesfamichael Taffere <149233683+Tesfamichael12@users.noreply.github.com>
2024-08-29 06:38:38 +0300

    Time: 72 ms (60.09%) | Memory: 16.5 MB (66.23%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.py
@@ -4,8 +4,9 @@ class Solution:
         new = sorted(nums)
         out = []
         
+        print(new)
         for num in nums:
             
-            out.append(new.index(num))
+            out.append(new.index(num)) # the index method returns the first occurance of num, which assures that it will append only the numbers less than num if duplicates exist
         
         return out
