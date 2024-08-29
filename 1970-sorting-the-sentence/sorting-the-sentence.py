4d262cdaea307fbe6b67256d4add328e65943ba5
Tesfamichael Taffere <149233683+Tesfamichael12@users.noreply.github.com>
2024-08-29 07:32:00 +0300

    Time: 42 ms (9.41%) | Memory: 16.4 MB (60.01%) - LeetSync

--- 1970-sorting-the-sentence/sorting-the-sentence.py
+++ 1970-sorting-the-sentence/sorting-the-sentence.py
@@ -7,7 +7,7 @@ class Solution:
         for ch in sen:
             count[ch[-1]] = ch[:-1]
         
-        new = dict(sorted(count.items()))
+        new = dict(sorted(count.items())) # if the key is not cased to int it will be a string so we can sort it by this methond i.e count.items() but if it was casted(count[int(ch[:-1])]) we can just sort it by saying new = dict(sorted(count))
 
         # n = list(new.values())
         # out = " ".join(n)
