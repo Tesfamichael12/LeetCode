5b640899f22ae6f076e1484df833b5c63111ebee
Tesfamichael Taffere <149233683+Tesfamichael12@users.noreply.github.com>
2024-08-29 08:00:49 +0300

    Time: 37 ms (57.98%) | Memory: 16.6 MB (22.76%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,17 +3,12 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # using the dutch national flag algo
-        l, m, r = 0, 0, len(nums) -1 
+        count = Counter(nums)
+    
+        nums.clear()
+        nums.extend([0] * count[0])
+        nums.extend([1] * count[1])
 
-        while m <= r:
-            if nums[m] == 0:
-                nums[m], nums[l] = nums[l], nums[m]
-                m, l = m + 1, l + 1
-            elif nums[m] == 2:
-                nums[m], nums[r] = nums[r], nums[m]
-                r -=1
-            else:
-                m += 1
+        nums.extend([2] * count[2])
             
             
