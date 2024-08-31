33656988eb7b0b5f5047cf7788547ca59baf3e8d
Tesfamichael Taffere <149233683+Tesfamichael12@users.noreply.github.com>
2024-08-31 12:18:38 +0300

    Time: 43 ms (23.87%) | Memory: 16.6 MB (46.80%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -4,11 +4,13 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         n = len(m)
-        # first transpose the matrix
+        # swap vertically
+        l, r = 0, n-1
+        while l < r:
+            m[l], m[r] = m[r], m[l]
+            l, r = l+1, r-1
+        
+        # swap diagonal
         for i in range(n):
-            for j in range(i, n): # To avoid redundant swaps, start j from i
+            for j in range(i, n):
                 m[i][j], m[j][i] = m[j][i], m[i][j]
-                
-        # reverse every row
-        for i in range(n):
-            m[i].reverse()
