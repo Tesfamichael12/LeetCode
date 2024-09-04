9576e1682e45bb98abb6f4a199bdd7f6f62b5243
Tesfamichael Tafere <149233683+Tesfamichael12@users.noreply.github.com>
2024-09-04 23:28:37 +0300

    Time: 42 ms (14.67%) | Memory: 16.6 MB (39.13%) - LeetSync

--- 2284-smallest-value-of-the-rearranged-number/smallest-value-of-the-rearranged-number.py
+++ 2284-smallest-value-of-the-rearranged-number/smallest-value-of-the-rearranged-number.py
@@ -1,7 +1,7 @@
 class Solution:
     def smallestNumber(self, num: int) -> int:
         digits = []
-        isneg = 1 if num < 0 else 0
+        isneg =  num < 0 
         num = abs(num)
         while num:
             digit = num%10
@@ -12,20 +12,15 @@ class Solution:
         else:
             digits.sort()
             if len(digits) > 1 and digits[0] == 0:
-                l = r = 0
+                l = r = 0 
                 while r < len(digits):
                     if digits[r] != 0:
                         digits[l], digits[r] = digits[r], digits[l]
                         break
                     r += 1
 
-        print( digits)
         number = 0
         for digit in digits:
             number = number * 10 + digit
 
-        if isneg:
-            number = -number
-            return number
-        else:
-            return number
+        return -number if isneg else number
