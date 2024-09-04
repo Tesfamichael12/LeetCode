3811caa2ddb1f718c9eed06acdc1d6b8805a5845
Tesfamichael Tafere <149233683+Tesfamichael12@users.noreply.github.com>
2024-09-04 20:29:44 +0300

    Time: 691 ms (10.04%) | Memory: 35.8 MB (8.64%) - LeetSync

--- 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.py
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.py
@@ -2,7 +2,6 @@ class Solution:
     def maxIceCream(self, costs: List[int], coins: int) -> int:
         count = Counter(costs)
         count = dict(sorted(count.items()))
-        print(count)
         ic = 0
         for cost, freq in count.items():
             if coins >= cost * freq: 
