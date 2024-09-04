4414a69cd0caeca8dc4be959829399e5ad89dd0f
Tesfamichael Tafere <149233683+Tesfamichael12@users.noreply.github.com>
2024-09-05 00:38:14 +0300

    Time: 0 ms (100.00%) | Memory: 8.3 MB (64.96%) - LeetSync

--- 2284-smallest-value-of-the-rearranged-number/smallest-value-of-the-rearranged-number.cpp
+++ 2284-smallest-value-of-the-rearranged-number/smallest-value-of-the-rearranged-number.cpp
@@ -1,37 +1,19 @@
 class Solution {
 public:
     long long smallestNumber(long long num) {
-        vector<long long> digits;
-        bool isneg = num < 0;
-        num = abs(num);
+        string s = to_string(abs(num));
         
-        while (num) {
-            long long digit = num % 10;
-            digits.push_back(digit);
-            num /= 10;
-        }
-        
-        if (isneg) {
-            sort(digits.rbegin(), digits.rend()); 
+        if (num < 0) {
+            sort(s.begin(), s.end(), greater<char>()); // or sort(s.rbegin(), s.rend());
         } else {
-            sort(digits.begin(), digits.end()); 
-            if (digits.size() > 1 && digits[0] == 0) {
-                long long l = 0, r = 0;
-                while (r < digits.size()) {
-                    if (digits[r] != 0) {
-                        swap(digits[l], digits[r]);
-                        break;
-                    }
-                    r++;
-                }
-            }
-        }
-
-        long long number = 0;
-        for (long long digit : digits) {
-            number = number * 10 + digit;
+            sort(s.begin(), s.end());
         }
         
-        return isneg ? -number : number;
+        if (num > 0)
+        {
+            swap(s[0], s[s.find_first_not_of('0')]);
+        }
+        
+        return stoll(s) * (num < 0 ? -1 : 1);
     }
 };
