d24c59180aa4f6f92cdd254ca91c668121cf1695
Tesfamichael Tafere <149233683+Tesfamichael12@users.noreply.github.com>
2024-09-07 01:49:57 +0300

    Time: 52 ms (6.10%) | Memory: 16.6 MB (9.30%) - LeetSync

--- 1009-pancake-sorting/pancake-sorting.py
+++ 1009-pancake-sorting/pancake-sorting.py
@@ -6,9 +6,22 @@ class Solution:
             res.extend([i+1, n])
             
             # arr = arr[:i:-1] + arr[:i]
+
+            # First flip: bring the largest number to the front
             arr[:i+1] = reversed(arr[:i+1])
+
+            # Second flip: move the largest number to its final position
             arr[:n] = reversed(arr[:n])
-            # arr = arr[:i+1:-1] + arr[i+1:]
-            # arr = arr[:n+1:-1] + arr[n+1:]
         
         return res
+
+#NOTE
+"""
+First let's recap on what has been asked:
+
+   - The array arr contains unique integers, and these integers are a permutation of the numbers from 1 to len(arr).
+   - The length of the array is <= 100, meaning a brute-force approach is feasible.
+   - Any solution that sorts the array using at most 10 * arr.length flips will be considered correct.
+   
+The strategy involves moving the largest unsorted number (which is len(arr) at the beginning) to the front of the list by flipping the entire subarray up to that number. Then, we flip the array again to place this number in its correct sorted position.
+"""
