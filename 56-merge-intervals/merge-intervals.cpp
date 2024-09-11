4eed1bd46024198fba1c047b45418ccd53221136
Tesfamichael Tafere <149233683+Tesfamichael12@users.noreply.github.com>
2024-09-11 21:38:27 +0300

    Time: 383 ms (5.02%) | Memory: 22.5 MB (98.65%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,7 +1,11 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end());
+        // Sort by the first element of each interval
+        sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b) {
+            return a[0] < b[0];
+        });
+        
         int i = 1;
         while (i < intervals.size()) {
             if (intervals[i-1][1] >= intervals[i][0]) {
