5744d4440307c62a769110e58d09cc0ad8b11416
Tesfamichael Tafere <149233683+Tesfamichael12@users.noreply.github.com>
2024-09-13 12:56:19 +0300

    Time: 5 ms (25.82%), Space: 8.5 MB (29.68%) - LeetHub

---
+++ 3379-score-of-a-string/3379-score-of-a-string.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int sum = 0;
+        for (int i = 1; i < s.length(); i++)
+            sum += abs((int)(s[i-1]) - (int)(s[i]));
+
+        return sum;
+    }
+};
