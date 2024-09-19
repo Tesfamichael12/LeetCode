3b06edf697bc1de97f1133265b3c40ec328142fb
Tesfamichael Tafere <149233683+Tesfamichael12@users.noreply.github.com>
2024-09-19 12:45:44 +0300

    Time: 686 ms (56.83%) | Memory: 23.9 MB (44.52%) - LeetSync

--- 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.py
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.py
@@ -1,9 +1,3 @@
 class Solution:
     def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
-        if len(original) != n*m: return []
-        res = [[0]*n for i in range(m)]
-        for k, num in enumerate(original):
-            i = k // n
-            j = k % n
-            res[i][j] = num
-        return res
+       return [original[i*n : (i+1)*n] for i in range(m)] if len(original) == n*m else []
