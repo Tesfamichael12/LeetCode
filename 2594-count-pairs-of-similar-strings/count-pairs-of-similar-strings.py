7728219e6c1f1e73d3d9a4026ada2fef54964919
Tesfamichael Tafere <149233683+Tesfamichael12@users.noreply.github.com>
2024-09-25 12:38:46 +0300

    Time: 55 ms (89.73%) | Memory: 16.7 MB (18.83%) - LeetSync

---
+++ 2594-count-pairs-of-similar-strings/count-pairs-of-similar-strings.py
@@ -0,0 +1,10 @@
+class Solution:
+    def similarPairs(self, words: List[str]) -> int:
+        count = 0
+        freq = collections.defaultdict(int)
+
+        for word in words:
+            hashword = frozenset(word)
+            count += freq[hashword]
+            freq[hashword] += 1
+        return count
