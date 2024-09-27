62e9983ffe55d4f81150d203274b35baf89c7a07
Tesfamichael Tafere <149233683+Tesfamichael12@users.noreply.github.com>
2024-09-27 14:06:54 +0300

    Time: 48 ms (56.03%) | Memory: 16.9 MB (21.23%) - LeetSync

---
+++ 1310-watering-plants/watering-plants.py
@@ -0,0 +1,11 @@
+class Solution:
+    def wateringPlants(self, plants: List[int], capacity: int) -> int:
+        steps = 0
+        can = capacity
+        for i, plant in enumerate(plants):
+            if plant > can:
+                steps += i*2 
+                can = capacity
+            steps += 1
+            can -= plant
+        return steps         
