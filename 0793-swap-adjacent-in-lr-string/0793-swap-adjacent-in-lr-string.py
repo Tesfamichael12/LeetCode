bf5ca5616c2b3d1896b6a7526c6c2c8a6b04265f
Tesfamichael Tafere <149233683+Tesfamichael12@users.noreply.github.com>
2024-10-02 18:03:41 +0300

    Time: 44 ms (64.96%), Space: 17.9 MB (5.66%) - LeetHub

---
+++ 0793-swap-adjacent-in-lr-string/0793-swap-adjacent-in-lr-string.py
@@ -0,0 +1,16 @@
+class Solution:
+    def canTransform(self, start: str, end: str) -> bool:
+        s = [[start[i], i] for i in range(len(start)) if start[i] != 'X']
+        e = [[end[i], i] for i in range(len(end)) if end[i] != 'X']
+
+        print(e)
+
+        if len(s) != len(e): return False
+
+        for i in range(len(s)):
+            if (s[i][0] == 'R' and e[i][0] == 'R'  and s[i][1] > e[i][1]) or (s[i][0] == 'L' and e[i][0] == 'L' and s[i][1] < e[i][1]):
+                return False
+            elif s[i][0] != e[i][0]:
+                return False
+
+        return True 
