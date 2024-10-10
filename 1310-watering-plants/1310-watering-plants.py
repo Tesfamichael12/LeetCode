7bb1680d6cc4bbd908cf6f3fe3088e7487ab8515
Tesfamichael Tafere <149233683+Tesfamichael12@users.noreply.github.com>
2024-10-10 22:49:13 +0300

    Update 1310-watering-plants.py

--- 1310-watering-plants/1310-watering-plants.py
+++ 1310-watering-plants/1310-watering-plants.py
@@ -2,6 +2,7 @@ class Solution:
     def wateringPlants(self, plants: List[int], capacity: int) -> int:
         steps = 0
         can = capacity
+        
         for i, plant in enumerate(plants):
             if plant > can:
                 steps += i*2 
