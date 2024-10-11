0913777a936521378229d1e8067e4f3bb2b58377
Tesfamichael Tafere <149233683+Tesfamichael12@users.noreply.github.com>
2024-10-11 17:36:28 +0300

    Time: 147 ms (93.25%) | Memory: 28.1 MB (8.71%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -3,12 +3,6 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        n = len(nums)
-        k = k % n
-        rotated = [0] * n
-
-        for i in range(n):
-            rotated[(i+k) % n] = nums[i]
-
-        for i in range(n):
-            nums[i] = rotated[i]  
+        k = k % len(nums)
+        if k != 0:
+            nums[:k], nums[k:] = nums[-k:], nums[:-k]
