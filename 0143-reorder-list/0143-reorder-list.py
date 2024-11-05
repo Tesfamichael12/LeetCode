19a68a03371869d97a3ea0687ffc7deffedf1d9f
Tesfamichael Tafere <149233683+Tesfamichael12@users.noreply.github.com>
2024-11-05 17:07:22 +0300

    Time: 0 ms (100%), Space: 22.3 MB (98.96%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -5,28 +5,25 @@
 #         self.next = next
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
-        if not head:
-            return
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
         
-        def recursion(node: Optional[ListNode]) -> Optional[ListNode]:
-            if not node or not node.next:
-                return node
-            
-            lastNode = node
-            prevNode = None
-            
-            while lastNode.next:
-                prevNode = lastNode
-                lastNode = lastNode.next
-            
-            prevNode.next = None
-
-            remainingPart = node.next
-            
-            lastNode.next = recursion(remainingPart)
-            
-            node.next = lastNode
-
-            return node
-
-        recursion(head)
+        second = slow.next
+        prev = slow.next = None
+        while second:
+            temp = second.next
+            second.next = prev
+            prev = second
+            second = temp
+        
+        first, second = head, prev
+        while second:
+            temp1, temp2 = first.next, second.next
+            first.next = second
+            second.next = temp1
+            first, second = temp1, temp2
