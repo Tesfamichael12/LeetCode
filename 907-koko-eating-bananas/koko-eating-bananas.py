3ea713541421f362759d8f886bc1d0d858d0c7b1
Tesfamichael Tafere <149233683+Tesfamichael12@users.noreply.github.com>
2024-11-07 21:33:09 +0300

    Time: 169 ms (27.76%) | Memory: 17.8 MB (74.11%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,13 +1,14 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        def feasible(speed):
-            return sum(math.ceil(pile / speed) for pile in piles) <= h
         
-        l, r = 1, max(piles)
-        while l < r:
-            mid = l + (r - l) // 2
-            if feasible(mid):
-                r = mid
+        L, R = 1, max(piles)
+        while L <= R:
+            mid = L + (R - L) // 2  # avoid any overflow
+            
+            time = sum(math.ceil(pile / mid) for pile in piles)
+            if time <= h:
+                R = mid - 1
             else:
-                l = mid + 1
-        return l
+                L = mid + 1
+        return L
+            
