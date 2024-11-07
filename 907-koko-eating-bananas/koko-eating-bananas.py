40f8e2b8fc68613a44ad183a10f7714217fb7da0
Tesfamichael Tafere <149233683+Tesfamichael12@users.noreply.github.com>
2024-11-07 22:06:28 +0300

    Time: 167 ms (29.74%) | Memory: 17.7 MB (84.83%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,9 +1,9 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         
-        L, R = 1, max(piles)
+        L, R = 1, max(piles)     # max steps 30 => 10^9 / 2^30 = 1
         while L <= R:
-            mid = L + (R - L) // 2  # avoid any overflow
+            mid = L + (R - L) // 2  
             
             time = sum(math.ceil(pile / mid) for pile in piles)
             if time <= h:
