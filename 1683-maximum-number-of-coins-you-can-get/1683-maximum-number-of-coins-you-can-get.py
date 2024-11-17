d9f9cfc31295e16bc37f9eedc3b69e0281e24907
Tesfamichael Tafere <149233683+Tesfamichael12@users.noreply.github.com>
2024-11-17 22:28:52 +0300

    Time: 59 ms (95.76%), Space: 27 MB (66.08%) - LeetHub

--- 1683-maximum-number-of-coins-you-can-get/1683-maximum-number-of-coins-you-can-get.py
+++ 1683-maximum-number-of-coins-you-can-get/1683-maximum-number-of-coins-you-can-get.py
@@ -1,8 +1,11 @@
 class Solution:
     def maxCoins(self, piles: List[int]) -> int:
+        # first determine how many piles each person will get 
+        piles_per_person = len(piles) // 3
+        # sort the piles in decending order 
         piles.sort(reverse=True)
-        print(piles)
-        steps = len(piles) // 3
-        score = piles[1:-steps:2]
-        print(score)
+        # Hypoteticaly we want to give the piles in the sorted pile for bob
+        # [9,8,7,6,5,4,3,2,1] 1, 2, 3 are given for bob
+        p = piles_per_person 
+        score = piles[1:-p:2] # starting from the second value and increment by 2 until we reach len(piles) - piles_per_person
         return sum(score)
