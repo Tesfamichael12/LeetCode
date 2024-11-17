1cc9e9f5395c63ccefd0303ce18c889d3f7784ae
Tesfamichael Tafere <149233683+Tesfamichael12@users.noreply.github.com>
2024-11-18 00:11:26 +0300

    Time: 0 ms (100%), Space: 16.6 MB (64.2%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,17 +3,14 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # using the dutch national flag algo
-        l, m, r = 0, 0, len(nums) -1 
-
-        while m <= r:
-            if nums[m] == 0:
-                nums[m], nums[l] = nums[l], nums[m]
-                m, l = m + 1, l + 1
-            elif nums[m] == 2:
-                nums[m], nums[r] = nums[r], nums[m]
-                r -=1
-            else:
-                m += 1
-            
-            
+        l, i, r = 0, 0, len(nums) - 1
+        while i <= r:
+            if nums[i] == 0:
+                nums[l], nums[i] = nums[i], nums[l]
+                i += 1
+                l += 1
+            elif nums[i] == 2:
+                nums[i], nums[r] = nums[r], nums[i]
+                r -= 1
+            else: # nums[i] == 1
+                i += 1
