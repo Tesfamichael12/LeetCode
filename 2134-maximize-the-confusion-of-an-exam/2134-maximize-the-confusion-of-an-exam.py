f5f7c9c6d29c333b8efa9891e32f85bf764fb056
Tesfamichael Tafere <149233683+Tesfamichael12@users.noreply.github.com>
2024-11-20 18:19:06 +0300

    Time: 244 ms (29.47%), Space: 16.9 MB (60.29%) - LeetHub

--- 2134-maximize-the-confusion-of-an-exam/2134-maximize-the-confusion-of-an-exam.py
+++ 2134-maximize-the-confusion-of-an-exam/2134-maximize-the-confusion-of-an-exam.py
@@ -11,7 +11,7 @@ class Solution:
             while r < n:
                 if answerKey[r] == ans:
                     flips -= 1
-                while flips < 0:
+                if flips < 0:
                     if answerKey[l] == ans:
                         flips += 1
                     l += 1
@@ -22,39 +22,3 @@ class Solution:
             return max_length
 
         return max(calculateLength('T'), calculateLength('F'))
-
-            
-
-        # max_length = 0
-        # flips = k
-        # n = len(answerKey)
-        # l, r = 0, 0
-        # # F -> T
-        # while r < n:
-        #     if answerKey[r] == 'F':
-        #         flips -= 1
-        #     while flips < 0:
-        #         if answerKey[l] == 'F':
-        #             flips += 1
-        #         l += 1
-
-        #     max_length = max(max_length, r - l + 1)
-        #     r += 1
-
-        # l, r = 0, 0
-        # # T -> F
-        # while r < n:
-        #     if answerKey[r] == 'T':
-        #         flips -= 1
-        #     while flips < 0:
-        #         if answerKey[l] == 'T':
-        #             flips += 1
-        #         l += 1
-
-        #     max_length = max(max_length, r - l + 1)
-        #     r += 1
-
-        # return max_length
-
-        
-            
