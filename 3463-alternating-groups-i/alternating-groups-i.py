5b134f0d12797427e2ae82c02af513208f3c5b1f
Tesfamichael Tafere <149233683+Tesfamichael12@users.noreply.github.com>
2024-11-23 13:58:38 -0800

    Time: 47 ms (86.71%) | Memory: 16.6 MB (28.23%) - LeetSync

--- 3463-alternating-groups-i/alternating-groups-i.py
+++ 3463-alternating-groups-i/alternating-groups-i.py
@@ -1,11 +1,13 @@
 class Solution:
     def numberOfAlternatingGroups(self, colors: List[int]) -> int:
+        n = len(colors) # keep the orginal length for latter
         count = 0
+
         # extend the colors array to simulate a circular pattern 
-        circular_colors = colors + colors[:2] # colors.extends(colors[:2])
-        
-        for r in range(len(colors)): 
-            if circular_colors[r] == circular_colors[r+2] and circular_colors[r] != circular_colors[r+1]:
+        colors.extend(colors[:2])
+
+        for r in range(n): 
+            if colors[r] == colors[r+2] and colors[r] != colors[r+1]:
                 count += 1
                 
         return count
