0cdbf60911801ac81682118508ef140cdfdd59d5
Tesfamichael Tafere <149233683+Tesfamichael12@users.noreply.github.com>
2024-11-27 17:54:04 +0300

    Time: 448 ms (89.24%) | Memory: 20.3 MB (43.95%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -8,22 +8,22 @@ class Solution:
                 break
             if i > 0 and nums[i] == nums[i-1]:
                 continue
-            
+
             L, R = i + 1, n - 1
             while L < R:
-                sum = nums[i] + nums[L] + nums[R]
-                if sum == 0:
+                threeSum = nums[i] + nums[L] + nums[R]
+                if threeSum == 0:
                     res.append([nums[i], nums[L], nums[R]])
-                    L +=1 
-                    R -=1
+                    L += 1
+                    R -= 1
+
                     while L < R and nums[L] == nums[L-1]:
                         L += 1
                     while L < R and nums[R] == nums[R+1]:
                         R -= 1
-                    
-                elif sum > 0:
+                elif threeSum > 0:
                     R -= 1
                 else:
                     L += 1
-    
+        
         return res
