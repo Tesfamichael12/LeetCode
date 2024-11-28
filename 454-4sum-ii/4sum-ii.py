3b9406586aaefc472c93ff959773116ea7eeb87e
Tesfamichael Tafere <149233683+Tesfamichael12@users.noreply.github.com>
2024-11-29 00:23:16 +0300

    Time: 340 ms (59.30%) | Memory: 17.8 MB (5.15%) - LeetSync

--- 454-4sum-ii/4sum-ii.py
+++ 454-4sum-ii/4sum-ii.py
@@ -3,10 +3,11 @@ from typing import List
 
 class Solution:
     def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
-        # Step 1: Count sums of pairs in nums1 and nums2
+        # Count sums of pairs in nums1 and nums2
         sum_count = Counter(a + b for a in nums1 for b in nums2)
+        print(sum_count)
 
-        # Step 2: Find complementary sums in nums3 and nums4
+        # Find complementary sums in nums3 and nums4
         count = 0
         for c in nums3:
             for d in nums4:
