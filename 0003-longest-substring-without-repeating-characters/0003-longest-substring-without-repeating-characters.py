cd25c129bf40443c4dade8282dc5a75941b9c178
Tesfamichael Tafere <149233683+Tesfamichael12@users.noreply.github.com>
2024-12-02 20:16:20 +0300

    Time: 19 ms (54.26%), Space: 17.4 MB (6.09%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,25 +1,18 @@
-class Solution(object):
-    def lengthOfLongestSubstring(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        maxlen = 0
         L = R = 0
-        maxlength = 0
         hashset = set()
         while R < len(s):
-            if len(s) == 1:
-                maxlength = 1
-            if s[R] in hashset: 
-                maxlength = max(maxlength, len(hashset))
-                while not(s[L] == s[R]): #if they are equal we need to move L only one step so the code outside the loop will be excuted L += 1 
+            if s[R] in hashset:
+                maxlen = max(maxlen, R - L) # maxlen = max(maxlen, len(hashset))
+                while s[L] != s[R]:
                     hashset.remove(s[L])
                     L += 1
-                hashset.remove(s[L])
-                L += 1 # if s[R] in hashset then we can move the pointer L all the way to i + 1 where i is the index of the first occurance of s[R]
-            else:
-                hashset.add(s[R])
-                maxlength = max(maxlength, len(hashset))
-                R += 1
-        return maxlength
+                L += 1
+
+            hashset.add(s[R])
+            R += 1
+            maxlen = max(maxlen, R - L)
         
+        return maxlen
