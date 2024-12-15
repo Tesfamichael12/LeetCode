a72f4822d04700c1729440f7775cae7839e53ae3
Tesfamichael Tafere <tesfamichael132@gmail.com>
2024-12-15 23:52:37 +0300

    Time: 15 ms (20.87%), Space: 18.5 MB (10.22%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,5 +1,15 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        n = len(nums) 
-        num = n*(n+1)//2 - sum(nums)
-        return num
+        my_dict = {}
+        min_val = min(nums)
+        for i in range(min_val, len(nums) + 1):
+            my_dict[i] = 0
+        
+        for num in nums:
+            my_dict[num] += 1
+        
+        for key, val in my_dict.items():
+            if val == 0:
+                return key
+        else:
+            return 0
