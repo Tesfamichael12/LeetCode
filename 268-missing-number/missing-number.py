45d82de93ec4ca60b535b893f2decdfe25b58f7f
Tesfamichael Tafere <tesfamichael132@gmail.com>
2024-12-15 23:53:53 +0300

    Time: 3 ms (64.82%) | Memory: 18.6 MB (5.74%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,15 +1,8 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        my_dict = {}
+        hashset = set(nums)
         min_val = min(nums)
         for i in range(min_val, len(nums) + 1):
-            my_dict[i] = 0
-        
-        for num in nums:
-            my_dict[num] += 1
-        
-        for key, val in my_dict.items():
-            if val == 0:
-                return key
-        else:
-            return 0
+            if i not in hashset:
+                return i
+        return 0
