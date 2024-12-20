039652b9208d97445969333b4b3ad44269fa9ce1
Tesfamichael Tafere <tesfamichael132@gmail.com>
2024-12-20 23:10:57 +0300

    Time: 190 ms (47.02%) | Memory: 23.7 MB (11.11%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.py
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -1,18 +1,20 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        L = 0
-        maxfruit = 0
-        hashmap = defaultdict(int)
-        for R in range(len(fruits)):
-            hashmap[fruits[R]] += 1
+        fruit_type = defaultdict(int)
+        maxlen = 0
+        l = 0
 
-            while len(hashmap) > 2:
-                f = fruits[L]
-                hashmap[f] -= 1
-                if not hashmap[f]: # or hashmap[f] == 0
-                    del hashmap[f] # or hashmap.pop(f)
-                L += 1
-            
-            maxfruit = max(maxfruit, R-L+1)
-        
-        return maxfruit
+        for r in range(len(fruits)):
+            fruit_type[fruits[r]] += 1
+
+            # keeping a valid window
+            while len(fruit_type) > 2:
+                fruit_type[fruits[l]] -= 1
+                
+                if fruit_type[fruits[l]] == 0:
+                    del fruit_type[fruits[l]]
+                l += 1
+
+            maxlen = max(maxlen, r - l + 1)
+
+        return maxlen
