a63756698cf2f5cb4a195e314cbe4e8a91f095b8
Tesfamichael Tafere <tesfamichael132@gmail.com>
2025-02-03 09:31:51 +0300

    Time: 0 ms (100%), Space: 17.7 MB (76.09%) - LeetHub

--- 0805-escape-the-ghosts/0805-escape-the-ghosts.py
+++ 0805-escape-the-ghosts/0805-escape-the-ghosts.py
@@ -1,8 +1,9 @@
 class Solution:
     def escapeGhosts(self, ghosts: List[List[int]], target: List[int]) -> bool:
-        steps = abs(0 - target[0]) + abs(0 - target[1])
-        for n in ghosts:
-            Gstep = abs(n[0] - target[0]) + abs(n[1] - target[1])
-            if Gstep <= steps:
+        steps = abs(target[0]) + abs(target[1])
+
+        for ghost in ghosts:
+            if abs(ghost[0] - target[0]) + abs(ghost[1] - target[1]) <= steps:
                 return False
+        
         return True
