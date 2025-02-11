894959a8f017d4a8030a2e10b4b2039114848702
Tesfamichael Tafere <tesfamichael132@gmail.com>
2025-02-11 12:25:45 +0300

    Time: 19 ms (45.51%), Space: 17.7 MB (68.62%) - LeetHub

--- 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.py
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,12 +1,8 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        sr = sorted(nums)
+        res = []
+        for n in nums:
+            res.append(sr.index(n))
 
-        new = sorted(nums)
-        out = []
-        
-        print(new)
-        for num in nums:
-            
-            out.append(new.index(num)) # the index method returns the first occurance of num, which assures that it will append only the numbers less than num if duplicates exist
-        
-        return out
+        return res
