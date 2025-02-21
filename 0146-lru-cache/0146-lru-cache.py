029c90f62c60408a58e45f724e8a9c007adc34bb
Tesfamichael Tafere <tesfamichael132@gmail.com>
2025-02-21 11:07:28 +0300

    Time: 119 ms (80.79%), Space: 78.3 MB (63.56%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,52 +1,24 @@
-class Node:
-    def __init__(self, key, val):
-        self.val = val
-        self.key = key
-        self.next = None
-        self.prev = None
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.cache = {}
         self.cap = capacity
-        self.least = Node(0, 0)
-        self.freq = Node(0, 0)
-        self.least.next, self.freq.prev = self.freq, self.least
-
-    # remove a node from our dublylinked list
-    def remove(self, node):
-        prev, nxt = node.prev, node.next
-        prev.next = nxt
-        nxt.prev = prev
-
-    # insert at the frequently used side of the linked list
-    def insert(self, node):
-        prev, nxt = self.freq.prev, self.freq
-
-        prev.next = nxt.prev = node
-        node.next, node.prev = nxt, prev
+        self.cache = {}
 
     def get(self, key: int) -> int:
         if key in self.cache: 
-            self.remove(self.cache[key])
-            self.insert(self.cache[key])
-
-            return self.cache[key].val
-        return -1
+            self.cache[key] = self.cache.pop(key)
+            return self.cache[key]
+        return - 1
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
-            self.remove(self.cache[key])
-
-        self.cache[key] = Node(key, value)
-        self.insert(self.cache[key])
+            self.cache.pop(key)
+        
+        self.cache[key] = value
         
         if len(self.cache) > self.cap:
-            # remove the LRU which is the least recently used 
-            lru = self.least.next
-            self.remove(lru)
-            del self.cache[lru.key]
+            self.cache.pop(next(iter(self.cache)))
 
 
 # Your LRUCache object will be instantiated and called as such:
