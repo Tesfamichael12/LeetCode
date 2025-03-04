6911100ab7574b81ba4089bf1ba7e60f92d0d6cc
Tesfamichael Tafere <tesfamichael132@gmail.com>
2025-03-04 18:50:50 +0300

    Time: 5 ms (43%), Space: 21.9 MB (79.54%) - LeetHub

--- 0890-lemonade-change/0890-lemonade-change.py
+++ 0890-lemonade-change/0890-lemonade-change.py
@@ -1,23 +1,24 @@
 class Solution:
     def lemonadeChange(self, bills: List[int]) -> bool:
-        f = t = 0
-        for n in bills:
-            if n == 5:
-                f += 1
-            elif n == 10:
-                if f > 0:
-                    f -= 1
-                    t += 1
+        change = defaultdict(int)
+        for bill in bills:
+
+            # validate
+            if bill == 10:
+                if change[5] < 1:
+                    return False
+
+                change[5] -= 1
+
+            elif bill == 20:
+                if change[10] >= 1 and change[5] >= 1:
+                    change[10] -= 1
+                    change[5] -= 1
+                elif change[5] >= 3:
+                    change[5] -= 3
                 else:
                     return False
-            else:
-                if f > 0 and t > 0:
-                    f -= 1
-                    t -= 1
-                elif f > 2:
-                    f -= 3
-                else:
-                    return False
-            
+
+            change[bill] += 1
+        
         return True
-            
