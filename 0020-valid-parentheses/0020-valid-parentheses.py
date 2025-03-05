7aa3d429ef550a257b7306e39a666566d5738dcc
Tesfamichael Tafere <tesfamichael132@gmail.com>
2025-03-05 16:50:56 +0300

    Time: 0 ms (100%), Space: 17.8 MB (51.05%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,13 +1,15 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        pr_dict = {')':'(', ']':'[', '}':'{'}
-        stack = []
-        for p in s:
-            if p in pr_dict.values():
-                stack.append(p)
-            elif stack and stack[-1] == pr_dict[p]:
-                stack.pop()
-            else: # if stack is empty but p is in pr_dict.keys() i.e we have extra closing brace
-                return False
+        par = { ']' :'[' ,  ')' : '(', '}' : '{' }
 
-        return stack == []
+        stack = []
+        for c in s:
+            if c in par:
+                if len(stack) == 0 or stack[-1] != par[c]:
+                    return False
+                else:
+                    stack.pop()
+            else:
+                stack.append(c)
+        
+        return len(stack) == 0
