5ee0e4cd71a2019d6c5fb41eeb750bb59df16bd3
Tesfamichael Tafere <tesfamichael132@gmail.com>
2025-03-08 17:54:03 +0300

    Time: 0 ms (100%), Space: 17.8 MB (41.9%) - LeetHub

--- 1033-broken-calculator/1033-broken-calculator.py
+++ 1033-broken-calculator/1033-broken-calculator.py
@@ -3,10 +3,9 @@ class Solution:
         steps = 0
         while t > val:
             if t % 2 == 0:
-                t //= 2  # Reverse doubling by halving t
+                t //= 2  
             else:
-                t += 1  # Make it even to prepare for division by 2
+                t += 1  
             steps += 1
         
-        # Now, we can only subtract to match val
-        return steps + (val - t)  # Decrement val to match t
+        return steps + (val - t)  
