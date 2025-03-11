e18fbce4c1252a47249c221a3e34ed16182b4c1b
Tesfamichael Tafere <tesfamichael132@gmail.com>
2025-03-11 18:53:54 +0300

    Time: 0 ms (100%), Space: 17.8 MB (50.33%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,23 +1,19 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        memo = dict()
-        def pow(x, n):
-            if (x, n) in memo:
-                return memo[(x, n)]
-                
-            if n == 0:
-                return 1
+        @cache
+        def pow(x,n):
+            if n < 1: return 1
+            if n == 1: return x
 
-            if n == 1:
-                return x
-                
-            if n % 2 == 0:    
-                memo[(x, n)] = pow(x, n // 2) * pow(x, n // 2)
+            if n % 2 == 0:
+                return pow(x, n // 2) * pow(x, n // 2)
             else:
-                memo[(x, n)] = pow(x, n // 2) * pow(x, n // 2 + 1)
-            return memo[(x, n)]
-        if n > 0:
-            return pow(x, n)
-        else:
-            res = pow(x, -n)
+                return pow(x, n // 2) * pow(x, n // 2 + 1)
+            
+        
+        res = pow(x, abs(n))
+
+        if n < 0:
             return 1 / res
+        
+        return res
