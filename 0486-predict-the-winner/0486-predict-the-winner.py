93538227a0a9a97d21cd75cf77cf35245de7798a
Tesfamichael Tafere <tesfamichael132@gmail.com>
2025-03-21 21:38:17 +0300

    Time: 3406 ms (11.32%), Space: 17.8 MB (73.74%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,25 +1,18 @@
 class Solution:
     def predictTheWinner(self, nums: List[int]) -> bool:
         memo = {}
-        def pridict(l, r, turn):
+        def pridict(l, r):
             if (l, r) in memo:
                 return memo[(l,r)]
 
             if l == r:
                 return nums[l]
             
-            if turn:
-                left = pridict(l+1, r, not turn) + nums[l]
-                right = pridict(l, r-1, not turn) + nums[r]
+            left = nums[l] - pridict(l + 1, r)
+            right = nums[r] - pridict(l, r - 1)
 
-                memo[(l,r)] = max(left, right)
-            else:
-                left = pridict(l+1, r, not turn) - nums[l]
-                right = pridict(l, r-1, not turn) - nums[r]
-            
-                memo[(l,r)] = min(left, right)
-            
-            return memo[(l,r)]
-            
-        res = pridict(0, len(nums) - 1, True)
+            return max(left, right)
+            return memo[(l, r)]
+           
+        res = pridict(0, len(nums) - 1)
         return True if res >= 0 else False
