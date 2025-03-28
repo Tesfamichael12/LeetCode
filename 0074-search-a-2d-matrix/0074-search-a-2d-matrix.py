e5ba6220384752d299ed8b8288616e0ccf719e87
Tesfamichael Tafere <tesfamichael132@gmail.com>
2025-03-29 00:54:15 +0300

    Time: 0 ms (100%), Space: 18.4 MB (25.26%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,28 +1,18 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        if len(matrix) == 1 :
-            return target in matrix[0]
-        if target < matrix[0][0] or target > matrix[-1][-1]:
-            return False
-        if len(matrix[0]) == 1:
-            l , r = 0 , len(matrix)-1
-            while l <= r:
-                mid = (l + r) // 2
-                if matrix[mid][0] == target:
-                    return True
-                elif matrix[mid][0] > target :
-                    r = mid - 1
-                else:
-                    l = mid + 1
-        for i in matrix:
-            l , r = 0 , len(i)  - 1
-            if target >= i[l] and target <= i[r]:
-                while l <= r:
-                    mid = (l + r) // 2
-                    if target == i[mid]:
-                        return True
-                    elif target < i[mid]:
-                        r = mid - 1
-                    else:
-                        l = mid + 1
+        row = len(matrix)
+        col = len(matrix[0])
+        l, r = 0, (row * col) - 1
+        while l <= r:
+            mid = (l+r)//2
+            # i = mid // col
+            # j = mid % col
+            i, j = divmod(mid, col) # one liner for the above
+            if matrix[i][j] == target:
+                return True
+            elif target >= matrix[i][j]:
+                l = mid + 1
+            else:
+                r = mid - 1
         return False
+
