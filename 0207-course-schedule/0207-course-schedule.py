53ae5cf78b8173e047a26babfe57758eea9c23dd
Tesfamichael Tafere <tesfamichael132@gmail.com>
2025-04-11 22:58:24 +0300

    Time: 7 ms (48.16%), Space: 19.1 MB (61.89%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -4,24 +4,22 @@ class Solution:
         for course, prereq in prerequisites:
             graph[course].append(prereq) # directed graph ( c -> p)
 
-        visited = set()
-        path = [0] * numCourses
+        visited = [-1] * numCourses # -1 = unvisited, 0 = visited not in path, 1 visited & in path
         def dfs(node):
-            visited.add(node)
-            path[node] = 1 # add to the current path
+            visited[node] = 1
 
             for adj in graph[node]:
-                if adj not in visited:
+                if visited[adj] == -1: # node is not visited
                     if dfs(adj):
                         return True
-                elif adj in visited and path[adj] == 1: # if adj is visited and is in the current path
+                elif visited[adj] == 1: # if adj is visited and is in the current path
                     return True
             
-            path[node] = 0 # remove the node from the path
+            visited[node] = 0 # remove the node from the path
             return False
 
         for i in range(numCourses):
-            if i not in visited:
+            if visited[i] == -1: # node is not visited
                 if dfs(i):
                     return False
         return True
