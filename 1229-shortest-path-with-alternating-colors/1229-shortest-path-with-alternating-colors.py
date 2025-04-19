bda29b1675ce5f75ae39d57cc7e0db6120fe883a
Tesfamichael Tafere <tesfamichael132@gmail.com>
2025-04-19 20:58:38 +0300

    Time: 3 ms (72.75%), Space: 18 MB (71.19%) - LeetHub

--- 1229-shortest-path-with-alternating-colors/1229-shortest-path-with-alternating-colors.py
+++ 1229-shortest-path-with-alternating-colors/1229-shortest-path-with-alternating-colors.py
@@ -6,19 +6,23 @@ class Solution:
         for u, v in blueEdges:
             graph[u][1].append(v)
         
-        qu = deque([(0, 0, 0), (0, 1, 0)])
+        qu = deque([(0, 0), (0, 1)])
         visited = set([(0, 0), (0, 1)])
 
         ans = [-1] * n
+        dist = 0
         while qu:
-            cur_node, cur_color, dist = qu.popleft()
-            print(qu)
-            if ans[cur_node] == -1 : ans[cur_node] = dist
+            for _ in range(len(qu)):
+                cur_node, cur_color = qu.popleft()
+                
+                if ans[cur_node] == -1 : ans[cur_node] = dist
 
-            next_color = 1 - cur_color
-            for adj in graph[cur_node][next_color]:
-                if (adj, next_color) not in visited:
-                    visited.add((adj, next_color))
-                    qu.append((adj, next_color, dist + 1))
+                next_color = 1 - cur_color
+                for adj in graph[cur_node][next_color]:
+                    if (adj, next_color) not in visited:
+                        visited.add((adj, next_color))
+                        qu.append((adj, next_color))
 
+            dist += 1
+        
         return ans
