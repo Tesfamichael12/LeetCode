aa0430fdde6b593655db9ea8a004801fbcf91ed0
Tesfamichael Tafere <tesfamichael132@gmail.com>
2025-05-15 13:05:49 +0300

    Time: 39 ms (16.19%), Space: 19 MB (97.7%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,8 +1,28 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        count = Counter(nums)
-        for n in nums:
-            if not(count[n] == 3):
-                return n
-        return -1 
-            
+        max_bit_lenght = 32
+
+        binary_rep = [0] * (max_bit_lenght)
+        for i in range(max_bit_lenght): # max lenght is 32
+
+            set_bit_count = 0
+            for num in nums:
+                if num < 0:
+                    num = (~num + 1)
+                if num & (1 << i):
+                    set_bit_count += 1
+
+            binary_rep[i] = set_bit_count % 3
+
+        binary_rep.reverse()
+        ans = int("".join(map(str, binary_rep)), 2) 
+        
+        cnt = 0
+        for num in nums:
+            if num == ans:
+                cnt += 1
+        
+        if cnt > 1 or cnt == 0:
+            return -ans
+        else:
+            return ans
